e76a9de82d16671ae4f2aad0cc5f75edb5a9b4bd
Roni Metsämuuronen <roni.metsamuuronen@hotmail.com>
2019-08-13 16:47:52 +0300

    Loading indicator

--- src/components/BulletinList.tsx
+++ src/components/BulletinList.tsx
@@ -3,36 +3,42 @@ import Bulletin from './Bulletin';
 
 type BulletinListState = {
   list: any[];
+  isReady: boolean;
 };
 
 export default class BulletinList extends React.Component<{}, BulletinListState> {
   state: Readonly<BulletinListState> = {
-    list: []
+    list: [],
+    isReady: false
   };
 
   componentDidMount() {
     fetch('http://localhost:3001/posts')
       .then(responce => responce.json())
-      .then(json => this.setState({ list: json }))
+      .then(json => this.setState({ list: json, isReady: true }))
       .catch(error => console.log(error));
   }
 
   public render() {
-    const { list } = this.state;
-    if (list.length > 0) {
-      return (
-        <div>
-          {list.map(bulletin => (
-            <Bulletin
-              header={bulletin.category}
-              message={bulletin.message}
-              ownerId={bulletin.ownerId}
-            />
-          ))}
-        </div>
-      );
+    const { list, isReady } = this.state;
+    if (isReady) {
+      if (list.length > 0) {
+        return (
+          <div>
+            {list.map(bulletin => (
+              <Bulletin
+                header={bulletin.category}
+                message={bulletin.message}
+                ownerId={bulletin.ownerId}
+              />
+            ))}
+          </div>
+        );
+      } else {
+        return <div>No Posts</div>;
+      }
     } else {
-      return <div>No Posts</div>;
+      return <div>Loading...</div>;
     }
   }
 }
