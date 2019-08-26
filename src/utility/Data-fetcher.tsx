847e31f8d2d85b6118af17457b5c4f38560badc7
Roni Metsämuuronen <roni.metsamuuronen@hotmail.com>
2019-08-26 11:01:05 +0300

    Add build to git ignore and prepare master for deployment by adding option for .env

--- src/utility/Data-fetcher.tsx
+++ src/utility/Data-fetcher.tsx
@@ -1,17 +1,19 @@
 import { Comment, Post, Categories } from '../Types';
 
+const URL = process.env.URL || 'http://localhost:3001';
+
 export const fetchUser = async (id: number) => {
-  const responce = await fetch(`http://localhost:3001/v1/users/${id}`).then(res => res.json());
+  const responce = await fetch(`${URL}/v1/users/${id}`).then(res => res.json());
   return responce;
 };
 
 export const fetchPost = async (id: string) => {
-  const responce = await fetch(`http://localhost:3001/v1/posts/${id}`).then(res => res.json());
+  const responce = await fetch(`${URL}/v1/posts/${id}`).then(res => res.json());
   return responce;
 };
 
 export const createComment = (comment: Comment) => {
-  return fetch('http://localhost:3001/v1/comments', {
+  return fetch(`${URL}/v1/comments`, {
     method: 'POST',
     body: JSON.stringify({
       userId: comment.userId,
@@ -26,7 +28,7 @@ export const createComment = (comment: Comment) => {
 };
 
 export const createPost = (post: Post) => {
-  return fetch('http://localhost:3001/v1/posts', {
+  return fetch(`${URL}/v1/posts`, {
     method: 'POST',
     body: JSON.stringify({
       ownerId: post.ownerId,
@@ -42,13 +44,11 @@ export const createPost = (post: Post) => {
 };
 
 export const fetchComments = async (id: string) => {
-  const responce = await fetch(`http://localhost:3001/v1/posts/${id}/comments`).then(res =>
-    res.json()
-  );
+  const responce = await fetch(`${URL}/v1/posts/${id}/comments`).then(res => res.json());
   return responce;
 };
 
 export const fetchPosts = async () => {
-  const responce = await fetch('http://localhost:3001/v1/posts').then(res => res.json());
+  const responce = await fetch(`${URL}/v1/posts`).then(res => res.json());
   return responce;
 };
