91cd931706e0d08c0bec5bd5bbffffc17576cdfe
Roni Metsämuuronen <roni.metsamuuronen@hotmail.com>
2019-08-26 12:30:19 +0300

    Fixed the environmental variable name for url

--- src/utility/Data-fetcher.tsx
+++ src/utility/Data-fetcher.tsx
@@ -1,19 +1,22 @@
 import { Comment, Post, Categories } from '../Types';
+import * as dotenv from 'dotenv';
 
-const URL = process.env.URL;
+dotenv.config();
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001';
+console.log(process.env.REACT_APP_API_URL);
 
 export const fetchUser = async (id: number) => {
-  const responce = await fetch(`${URL}/v1/users/${id}`).then(res => res.json());
+  const responce = await fetch(`${API_URL}/v1/users/${id}`).then(res => res.json());
   return responce;
 };
 
 export const fetchPost = async (id: string) => {
-  const responce = await fetch(`${URL}/v1/posts/${id}`).then(res => res.json());
+  const responce = await fetch(`${API_URL}/v1/posts/${id}`).then(res => res.json());
   return responce;
 };
 
 export const createComment = (comment: Comment) => {
-  return fetch(`${URL}/v1/comments`, {
+  return fetch(`${API_URL}/v1/comments`, {
     method: 'POST',
     body: JSON.stringify({
       userId: comment.userId,
@@ -28,7 +31,7 @@ export const createComment = (comment: Comment) => {
 };
 
 export const createPost = (post: Post) => {
-  return fetch(`${URL}/v1/posts`, {
+  return fetch(`${API_URL}/v1/posts`, {
     method: 'POST',
     body: JSON.stringify({
       ownerId: post.ownerId,
@@ -44,11 +47,11 @@ export const createPost = (post: Post) => {
 };
 
 export const fetchComments = async (id: string) => {
-  const responce = await fetch(`${URL}/v1/posts/${id}/comments`).then(res => res.json());
+  const responce = await fetch(`${API_URL}/v1/posts/${id}/comments`).then(res => res.json());
   return responce;
 };
 
 export const fetchPosts = async () => {
-  const responce = await fetch(`${URL}/v1/posts`).then(res => res.json());
+  const responce = await fetch(`${API_URL}/v1/posts`).then(res => res.json());
   return responce;
 };
